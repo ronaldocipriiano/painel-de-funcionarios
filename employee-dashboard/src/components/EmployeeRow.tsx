1c679397248150f53f1bf899b8f24498c8a6e5f7
Ronaldo Cipriano <ronaldolopes14@gmail.com>
2024-06-23 11:02:47 -0300

    Implementando formatação de datas e telefone

--- employee-dashboard/src/components/EmployeeRow.tsx
+++ employee-dashboard/src/components/EmployeeRow.tsx
@@ -4,6 +4,14 @@ interface EmployeeRowProps {
   employee: Employee;
 }
 
+const formatDate = (date: string) => {
+  return new Date(date).toLocaleDateString();
+}
+
+const formatPhone = (phone: string) => {
+  return phone.replace(/(\d{2})(\d{2})(\d{5})(\d{4})/, '+$1 ($2) $3-$4');
+};
+
 const EmployeeRow: React.FC<EmployeeRowProps> = ({ employee }) => {
   return (
     <tr>
@@ -12,8 +20,8 @@ const EmployeeRow: React.FC<EmployeeRowProps> = ({ employee }) => {
       </td>
       <td>{employee.name}</td>
       <td>{employee.job}</td>
-      <td>{new Date(employee.admission_date).toLocaleDateString()}</td>
-      <td>{employee.phone}</td>
+      <td>{formatDate(employee.admission_date)}</td>
+      <td>{formatPhone(employee.phone)}</td>
     </tr>
   );
 };
